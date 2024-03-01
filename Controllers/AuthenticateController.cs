29df3ad777a5be6cc7ac2d48e732cdfb62b914e8
Giang Le <81180883+letruonggiangnh@users.noreply.github.com>
2024-03-01 11:27:13 +0700

    Update AuthenticateController.cs
    OK

--- Controllers/AuthenticateController.cs
+++ Controllers/AuthenticateController.cs
@@ -69,7 +69,7 @@ namespace ApiAppBanSach.Controllers
                     var authClaims = new List<Claim>
                     {
                         new Claim(ClaimTypes.Name, user.UserName),
-                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
                     };
 
                     foreach (var userRole in userRoles)
